346869f4715a6afc43e7cc0a9040600e5e4823cc
Alin Florin <alin@qoffice.ro>
2022-01-05 23:16:40 +0200

    d

--- stg/main.tf
+++ stg/main.tf
@@ -16,26 +16,3 @@ terraform {
 provider "azurerm" {
   features {}
 }
-
-locals {
-  secrets = tomap({
-
-  })
-}
-
-module "eh_ad_audit" {
-  source         = "../modules/eh"
-  location       = var.location
-  name           = "eh-${var.app_name}-ad-audit-${var.env}"
-  resource_group = var.resource_group
-  capacity       = null
-  sku            = "Basic"
-  zone_redundant = true
-}
-
-module "ad_diag_setting" {
-  source                         = "../modules/ad-diag-setting"
-  name                           = "eh-audit-${var.env}"
-  eventhub_authorization_rule_id = module.eh_ad_audit.authorization_rule_id
-  event_hub_name                 = module.eh_ad_audit.name
-}
