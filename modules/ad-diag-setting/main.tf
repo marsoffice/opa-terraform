d87cce45e4d184a4c7e5a5d6bb75039848deb564
Alin Florin <alin@qoffice.ro>
2022-01-07 18:02:18 +0200

    -

--- modules/ad-diag-setting/main.tf
+++ modules/ad-diag-setting/main.tf
@@ -5,7 +5,7 @@ resource "azurerm_monitor_aad_diagnostic_setting" "ad_diag_setting" {
     enabled  = true
     retention_policy {
       enabled = true
-      days    = 7
+      days    = 1
     }
   }
   storage_account_id = var.storage_account_id
